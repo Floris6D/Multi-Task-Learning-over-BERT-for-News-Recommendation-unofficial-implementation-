a21b8c28593d59976fd659e97e8ff04f1988fc09
JasperEppink <jasper.eppink@gmail.com>
2024-06-06 19:50:07 +0200

    Update dataloader

--- src/mtrec/NeRD_data.py
+++ src/mtrec/NeRD_data.py
@@ -4,12 +4,17 @@ import numpy as np
 from pathlib import Path
 import polars as pl
 
-from ebrec.utils._polars import slice_join_dataframes
+from ebrec.utils._polars import slice_join_dataframes, concat_str_columns
 from ebrec.utils._behaviors import (
     create_binary_labels_column,
     sampling_strategy_wu2019,
     truncate_history,
 )
+from ebrec.utils._python import (
+    repeat_by_list_values_from_matrix,
+    create_lookup_objects,
+)
+from ebrec.utils._articles import create_article_id_to_value_mapping
 
 from torch.utils.data import Dataset
 
@@ -26,40 +31,58 @@ class EB_NeRDDataset(Dataset):
             
         # Now load the data
         self.load_data()
+        
+        # Now tokenize the data and create the lookup tables
+        
     
     def __len__(self):
-        return len(self.df_behaviors)
+        return len(self.full_behaviors)
 
     def __getitem__(self, index):
         # Get the title and label
-        title = self.full_behaviors['title'].iloc[index]
-        label = ...
+        inview = self.full_behaviors[index]['article_ids_inview']
+        target = self.full_behaviors[index]['article_ids_clicked']
   
-        return title, label
+        return inview, target
     
     def load_data(self):
         FULL_PATH = os.path.join(self.data_dir, self.split)
         
         # Load the data
-        df_behaviors = pl.scan_parquet(os.path.join(FULL_PATH, 'behaviors.parquet'))
-        df_history = pl.scan_parquet(os.path.join(FULL_PATH, 'history.parquet'))
-        df_history = df_history.pipe(
-                        truncate_history,
-                        column='article_id_fixed',
-                        history_size=self.history_size,
-                        padding_value=0,
-                        enable_warning=False,
-                    )
+        df_history = (
+            pl.scan_parquet(os.path.join(FULL_PATH, 'history.parquet'))
+            .pipe(
+                truncate_history,
+                column='article_id_fixed',
+                history_size=self.history_size,
+                padding_value=0,
+                enable_warning=False,
+            )
+        )
         
-        self.full_behaviors = slice_join_dataframes(
-                df1=df_behaviors.collect(),
+        self.df_behaviors = (
+            pl.scan_parquet(os.path.join(FULL_PATH, 'behaviors.parquet'))
+            .collect()
+            .pipe(
+                slice_join_dataframes,
                 df2=df_history.collect(),
                 on='user_id',
-                how="left",
+                how='left',
             )
+        )
         
         # Load the article data
         self.df_articles = pl.scan_parquet(os.path.join(self.data_dir, 'articles.parquet'))
+        
+    def tokenize_data(self):
+        # IDK TODO uitzoeken wat dit doet
+        df_articles, cat_cal = concat_str_columns(df = self.df_articles, columns=['subtitle', 'title'])
+        
+        # Now create lookup tables
+        article_mapping = create_article_id_to_value_mapping(df=df_articles, value_col=)
+        self.lookup_article_index, self.lookup_article_matrix = create_lookup_objects(
+            
+        )
         
     
     
