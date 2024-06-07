88ec4e631fe28999bb0787cc2bb58f37f5b27d0b
Floris6D <florissixdijkstra@gmail.com>
2024-06-07 15:45:26 +0200

    added more to the skeleton of news_encoder.py

--- src/mtrec/news_encoder.py
+++ src/mtrec/news_encoder.py
@@ -1,11 +1,12 @@
 import torch
 
 class NewsEncoder(torch.nn.Module):
-    def __init__(self, tok_size, embedding_dim, num_classes):
+    def __init__(self, tok_size, embedding_dim, num_classes, num_ner):
         super(NewsEncoder, self).__init__()
+        #TODO: add configuration for the number of layers and hidden size
         self.cat_net = self.initialize_network(tok_size, num_classes)
-        self.ner_net = ...
-        self.bert = ...
+        self.ner_net = self.initialize_network(tok_size, num_ner)
+        self.bert = ... #TODO: Add BERT model
 
     def initialize_network(self, input_size, output_size, hidden_size=124, num_layers=2, act_fn = torch.nn.ReLU()):
         if num_layers < 1:
@@ -19,9 +20,21 @@ class NewsEncoder(torch.nn.Module):
             input_size = hidden_size
         layers.append(torch.nn.Linear(hidden_size, output_size))
         return torch.nn.Sequential(*layers)
-        
+
+    def forward_cat(self, x):
+        cls_embedding = ...
+        return self.cat_net(cls_embedding)
+
+    def forward_ner(self, x):
+        title_embeddings = ...
+        result = []
+        for e in title_embeddings:
+            result.append(self.ner_net(e))
+        return result
+
+
     def forward(self, x):
         x = self.bert(x)
-        cat = self.cat_net(x)
-        ner = self.ner_net(x)
+        cat = self.forward_cat(x)
+        ner = self.forward_ner(x)
         return x, cat, ner
