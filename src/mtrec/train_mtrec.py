5be7d342fa0d289e3835a6d07ee7a51ede53a7f8
Floris6D <florissixdijkstra@gmail.com>
2024-06-18 17:33:59 +0200

    drop last is true

--- src/mtrec/train_mtrec.py
+++ src/mtrec/train_mtrec.py
@@ -9,6 +9,7 @@ from transformers import BertTokenizer, BertModel
 from user_encoder import UserEncoder
 from news_encoder import NewsEncoder
 from trainer import train
+#from peft import LoraConfig, get_peft_model
 
 def load_configuration(config):
     file_path = f'src/mtrec/configs/{config}.yml'
@@ -20,7 +21,7 @@ def get_dataloaders(cfg):
     tokenizer = BertTokenizer.from_pretrained(cfg['model']['pretrained_model_name'])
     return (DataLoader(
                     EB_NeRDDataset(tokenizer, **cfg['dataset'], split=split, override_eval2false= True),
-                    batch_size=cfg["trainer"]["batch_size"], shuffle=True, num_workers=16) 
+                    batch_size=cfg["trainer"]["batch_size"], shuffle=True, num_workers=16, drop_last=-True) 
                     for split in ['train', 'validation'] #TODO: add test, need to download
                     )	
 
@@ -29,8 +30,11 @@ def main():
     parser.add_argument('--file', default='test', help='Path to the configuration file')
     args = parser.parse_args()
     cfg = load_configuration(args.file)
-        
+    
+
     bert = BertModel.from_pretrained(cfg['model']['pretrained_model_name'])
+    #bert = get_peft_model(bert, LoraConfig(cfg["lora_config"]))
+    
     user_encoder = UserEncoder(**cfg['user_encoder'])
     news_encoder = NewsEncoder(**cfg['news_encoder'], bert=bert)
     
