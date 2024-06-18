3e39ea1e67f49276c68585948a018cdf1a779fe6
Floris6D <florissixdijkstra@gmail.com>
2024-06-18 16:50:47 +0200

    attempt to fix big that occurs with changing max title length

--- src/mtrec/trainer.py
+++ src/mtrec/trainer.py
@@ -81,12 +81,12 @@ def NER_loss(p1, p2, l1, l2, mask1, mask2):
     """
     bs, N1, tl1, num_ner = p1.shape
     bs, N2, tl2, num_ner = p2.shape
-    print(p1.shape, p2.shape)
-    print(l1.shape, l2.shape)   
+    # print(p1.shape, p2.shape)
+    # print(l1.shape, l2.shape)   
     p1 = p1.reshape(bs*N1*tl1, num_ner)
     p2 = p2.reshape(bs*N2*tl2, num_ner)
-    l1 = l1.reshape(bs*N1*tl1)
-    l2 = l2.reshape(bs*N2*tl2)
+    l1 = l1[:,:,:tl1].reshape(bs*N1*tl1)
+    l2 = l2[:,:,:tl2].reshape(bs*N2*tl2)
        
     mask1 = mask1[:,:,:tl1].reshape(bs*N1*tl1)
     mask2 = mask2[:,:,:tl2].reshape(bs*N2*tl2)
