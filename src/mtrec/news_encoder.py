399ee1ba113f1d40715ab8240ea1afe27b3d1664
JasperEppink <jasper.eppink@gmail.com>
2024-06-20 17:34:08 +0200

    Bugfix and nans when no inview articles

--- src/mtrec/news_encoder.py
+++ src/mtrec/news_encoder.py
@@ -25,13 +25,29 @@ class NewsEncoder(torch.nn.Module):
         return torch.nn.Sequential(*layers)
 
     
-    def forward_cat(self, last_hidden_state):
-        cls_tokens = last_hidden_state[:, 0, :]
-        logits = self.cat_net(cls_tokens)
-        return torch.nn.functional.softmax(logits, dim=1)
+    def forward_cat(self, last_hidden_state, mask):
+        
+        # Last hidden state has dimensions (batch_size, max_inview_articles, max_title_length, hidden_size)
+        cls_tokens = last_hidden_state[:, :, 0, :]
+        bs, n, _ = cls_tokens.shape
+        cls_tokens = cls_tokens.reshape(bs*n, -1) # Reshape to (batch_size*max_inview_articles, hidden_size)
+        
+        # Again remove all rows with only zeros in the mask
+        subset_idx = mask.sum(dim=1) > 0
+        used_tokens = cls_tokens[subset_idx]
+        
+        # Get the category predictions logits
+        logits = self.cat_net(used_tokens)
+        
+        # Now reshape the logits back to the original shape fill with nas when no data of batch size inview articles using the reversed subset_idx
+        final_logits = torch.nan*torch.ones(bs*n, logits.shape[1])
+        final_logits[subset_idx] = logits
+        logits = final_logits.reshape(bs, n, -1)
+        
+        return torch.nn.functional.softmax(logits, dim=2)
 
     
-    def forward_ner(self, last_hidden_state):
+    def forward_ner(self, last_hidden_state, mask):
         sentence_tokens = last_hidden_state[:, 1:-1, :] #all tokens but SEP and CLS #TODO: check how this works with padding
         logits =  self.ner_net(sentence_tokens) 
         output = torch.nn.functional.softmax(logits, dim=2)
@@ -47,9 +63,9 @@ class NewsEncoder(torch.nn.Module):
         # Next remove all rows with only zeros in the mask
         subset_idx = mask.sum(dim=1) > 0
         tokens = tokens[subset_idx]
-        mask = mask[subset_idx]
+        bert_mask = mask[subset_idx]
         
-        x = self.bert(tokens, mask)
+        x = self.bert(tokens, bert_mask)
         last_hidden_state = x.last_hidden_state
         news_embeddings = last_hidden_state[:, 0 , :] #CLS token at index 0
         
@@ -64,9 +80,9 @@ class NewsEncoder(torch.nn.Module):
         # Also restructure the last_hidden_state and use the reversed subset_idx
         final_last_hidden_state = torch.zeros(bs*n, last_hidden_state.shape[1], last_hidden_state.shape[2])
         final_last_hidden_state[subset_idx] = last_hidden_state
-        final_last_hidden_state = final_last_hidden_state.reshape(bs, n, ts, -1) 
+        final_last_hidden_state = final_last_hidden_state.reshape(bs, n, ts, -1) # Batch size, max inview articles, max title length, hidden size (BERT)
         
         # Get the category and ner predictions # TODO: JE Check if this deals correctly with padding
-        cat = self.forward_cat(final_last_hidden_state).reshape(bs, n, -1)
-        ner = self.forward_ner(final_last_hidden_state).reshape(bs, n, -1, self.num_ner)        
+        cat = self.forward_cat(final_last_hidden_state, mask).reshape(bs, n, -1)
+        ner = self.forward_ner(final_last_hidden_state, mask).reshape(bs, n, -1, self.num_ner)        
         return final_news_embeddings, cat, ner
