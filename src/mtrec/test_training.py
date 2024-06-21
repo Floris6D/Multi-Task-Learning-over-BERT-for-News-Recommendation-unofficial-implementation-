67857ecc70adad465bd1b27807049edca44be216
JasperEppink <jasper.eppink@gmail.com>
2024-06-21 13:06:52 +0200

    Test

--- src/mtrec/test_training.py
+++ src/mtrec/test_training.py
@@ -13,6 +13,32 @@ Mtrec_model = Mtrec(cfg, device="cpu").to(device)
 
 # Just for debugging this script
 print("Check 1")
-_ = Mtrec_model.train(dataloader_train)
+#initialize optimizer
+user_encoder = Mtrec_model.user_encoder
+news_encoder = Mtrec_model.news_encoder
+params = [
+    {"params": [user_encoder.W,  user_encoder.q],   "lr": cfg["lr_user"]},  # lr of attention layer in user encoder
+    {"params": list(news_encoder.cat_net.parameters()) + list(news_encoder.ner_net.parameters()),
+                "lr": cfg["lr_news"]},  # lr of auxiliary tasks
+    {"params": news_encoder.bert.parameters(), "lr": cfg["lr_bert"]}  # Parameters of BERT
+] 
 
+if cfg["optimizer"] == "adam":
+    optimizer = torch.optim.Adam(params)
+elif cfg["optimizer"] == "sgd":
+    optimizer = torch.optim.SGD(params)
+elif cfg["optimizer"] == "adamw":
+    optimizer = torch.optim.AdamW(params)
+else:
+    print("Invalid optimizer <{}>.".format(cfg["optimizer"]))
+
+#TURNEMALLON TODO: remove this?
+for param_group in optimizer.param_groups:
+    for param in param_group['params']:
+        param.requires_grad=True
+
+print_flag = True
+total_loss, total_main_loss = Mtrec_model.train(dataloader_train, optimizer, print_flag)
+
+test = 7
 
