00a9980be55579998d8bd1be07f3e4caea59752f
Floris6D <florissixdijkstra@gmail.com>
2024-06-24 13:28:06 +0200

    error except improvement in hypertuning

--- src/mtrec/hypertuning.py
+++ src/mtrec/hypertuning.py
@@ -8,15 +8,18 @@ from utils import load_configuration, get_dataloaders
 import copy
 from mtrec_class import Mtrec
 import torch
+import time
 
 
 def test_config(trial, cfg, device):
+    start = time.time()
     cfg = copy.deepcopy(cfg)
     hcf = cfg["hypertuning"]
     hidden_sizes = hcf["hidden_size"]
     nl_min, nl_max = hcf["num_layers"]["min"],hcf["num_layers"]["max"]
     lr_min, lr_max = hcf["lr"]["min"], hcf["lr"]["max"]
     batch_sizes = hcf["batch_size"]
+    optimizers = hcf["optimizer"]
     # Categorical net
     cfg["news_encoder"]["cfg_cat"]["hidden_size"] = trial.suggest_categorical("cat_hidden_size", hidden_sizes)
     cfg["news_encoder"]["cfg_cat"]["num_layers"] = trial.suggest_int("cat_num_layers", nl_min, nl_max)
@@ -32,17 +35,23 @@ def test_config(trial, cfg, device):
     cfg["trainer"]["lr_news"] = trial.suggest_float("lr_news", lr_min, lr_max, log=True)
     cfg["trainer"]["lr_bert"] = trial.suggest_float("lr_bert", lr_min, lr_max, log=True)
     
+    cfg["trainer"]["optimizer"] = trial.suggest_categorical("optimizer", optimizers)
     Mtrec_model = Mtrec(cfg, device=device)
     
     dataloaders = get_dataloaders(cfg)
-    (dataloader_train, dataloader_val) = (dataloaders[0], dataloaders[1])
-    model, best_validation_loss =       train(model     = Mtrec_model, 
-                                       dataloader_train = dataloader_train, 
-                                       dataloader_val   = dataloader_val, 
-                                       cfg              = cfg["trainer"], 
-                                       device           = device)
-    
-    return best_validation_loss
+    try:    
+        (dataloader_train, dataloader_val) = (dataloaders[0], dataloaders[1])
+        model, best_validation_loss =       train(model     = Mtrec_model, 
+                                        dataloader_train = dataloader_train, 
+                                        dataloader_val   = dataloader_val, 
+                                        cfg              = cfg["trainer"], 
+                                        device           = device)
+    except Exception as e:
+        print(f"EXCEPTION AS : {e}")
+        best_validation_loss = 1000
+    finally:
+        print(f"Time taken for trial: {time.time()-start}")
+        return best_validation_loss
 
 def main():
     parser = argparse.ArgumentParser(description='Process some arguments.')
@@ -53,7 +62,7 @@ def main():
     device =  "cuda" if torch.cuda.is_available() else "cpu"
     target_func = partial(test_config, cfg = cfg, device=device)
 
-    study = optuna.create_study()
+    study = optuna.create_study(direction = "minimize")
     study.optimize(target_func, n_trials=100)
 
     print("best parameters:\n", study.best_params)
