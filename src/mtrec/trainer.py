1817eadbd676909c9e2448577a23833d90bcd314
Floris6D <florissixdijkstra@gmail.com>
2024-06-26 16:53:38 +0200

    debug

--- src/mtrec/trainer.py
+++ src/mtrec/trainer.py
@@ -1,10 +1,9 @@
 import torch
 import os
 import copy
-from gradient_surgery2 import PCGrad
+from gradient_surgery import PCGrad
 from utils_training import *
 import time
-import wandb
 
     
 def train(model, dataloader_train, dataloader_val, cfg, 
